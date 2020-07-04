b23972605dfb2f74751503d3441a46ff8058221d
gargparthb <62770333+gargparthb@users.noreply.github.com>
2020-07-04 01:40:41 -0400

    logging in

--- public/JS/api-calls.js
+++ public/JS/api-calls.js
@@ -39,54 +39,51 @@ async function submitCreds() {
         const response = await fetch(`/api/${name}/${pin}`);
         const result = await response.json();
 
+        if (result.users.length == 0) {
+            logUser(name, pin);
+            account.currentUser = {
+                username: name,
+                pin: pin,
+                x2times: [],
+                x3times: [],
+                x4times: [],
+                x5times: []
+            };
+        } else {
+            account.currentUser = result.users[0];
+        }
 
         account.loggedIn = true;
         account.accBtnLabel = 'Statistics';
         loginBtn.html(account.accBtnLabel);
         populateStats();
-
-        if (result.users.length == 0) {
-            account.currentUser = logUser(name, pin);
-        } else {
-            account.currentUser = result.users[0];
-        }
+        switchAccView();
     }
 }
 
-function populateStats() {}
+function populateStats() {
+    _2best.html(best(account.currentUser.x2times));
+    _2bestAo5.html(bestAoX(account.currentUser.x2times, 5));
+    _2bestAo12.html(bestAoX(account.currentUser.x2times, 12));
+    _2mean.html(mean(account.currentUser.x2times));
 
-function best(array) {
-    return array.reduce(function (accumulator, currentValue) {
-        if (accumulator < currentValue) {
-            return accumulator;
-        } else {
-            return currentValue;
-        }
-    })
+    _3best.html(best(account.currentUser.x3times));
+    _3bestAo5.html(bestAoX(account.currentUser.x3times, 5));
+    _3bestAo12.html(bestAoX(account.currentUser.x3times, 12));
+    _3mean.html(mean(account.currentUser.x3times));
+
+    _4best.html(best(account.currentUser.x4times));
+    _4bestAo5.html(bestAoX(account.currentUser.x4times, 5));
+    _4bestAo12.html(bestAoX(account.currentUser.x4times, 12));
+    _4mean.html(mean(account.currentUser.x4times));
+
+    _5best.html(best(account.currentUser.x5times));
+    _5bestAo5.html(bestAoX(account.currentUser.x5times, 5));
+    _5bestAo12.html(bestAoX(account.currentUser.x5times, 12));
+    _5mean.html(mean(account.currentUser.x5times));
 }
 
-function bestAoX(array, x) {
-    let averages = [];
-    for (let i = 0; i < array.length - 1; i++) {
-        if (array[i + x - 1] != undefined) {
-            averages.push(aoX(array.slice(i, i + x), x));
-        }
-    }
-    return best(averages);
-}
-
-function aoX(array, x) {
-    let target = array;
-    target.sort((a, b) => a < b);
-    target.shift();
-    target.pop();
-    return target.reduce((acc, cv) => acc + cv) / (x - 2);
-}
-
-function mean(arr) {
-    return arr.reduce(reducer) / arr.length;
-
-    function reducer(a, b) {
-        return a + b;
-    }
+function switchAccView() {
+    loginContainer.style('display', 'none');
+    statContainer.style('display', 'flex');
 }
---
+++ public/JS/callbacks/stats.js
@@ -0,0 +1,50 @@
+function best(array) {
+    if (array.length == 0) {
+        return 'N/A';
+    } else {
+        let target = array.reduce(function (accumulator, currentValue) {
+            if (accumulator < currentValue) {
+                return accumulator;
+            } else {
+                return currentValue;
+            }
+        })
+    }
+}
+
+function bestAoX(array, x) {
+    let averages = [];
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i + x - 1] != undefined) {
+            averages.push(aoX(array.slice(i, i + x), x));
+        } else {
+            averages.push(null);
+        }
+    }
+
+    if (averages.includes(null)) {
+        return 'N/A';
+    } else {
+        return best(averages);
+    }
+}
+
+function aoX(array, x) {
+    let target = array;
+    target.sort((a, b) => a < b);
+    target.shift();
+    target.pop();
+    return target.reduce((acc, cv) => acc + cv) / (x - 2);
+}
+
+function mean(arr) {
+    if (arr.length == 0) {
+        return 'N/A';
+    } else {
+        return (arr.reduce(reducer) / arr.length).toFixed(1);
+    }
+
+    function reducer(a, b) {
+        return a + b;
+    }
+}
